c767c31ffe4db4bbeac5d673b40f86886cc89b85
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-05-04 23:05:51 +0530

    connected spring application to MySQL database

--- src/main/java/com/product_api/controllers/HomeController.java
+++ src/main/java/com/product_api/controllers/HomeController.java
@@ -1,10 +1,14 @@
 package com.product_api.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/home")
 public class HomeController {
-
+    @GetMapping
+    public String sayHello(){
+        return "Hello Nilesh";
+    }
 }
