74bc393faad796f00e3caf1b7168aefe82c0ab0b
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-05-05 23:47:54 +0530

    created controller method and mapped them with endpoints

--- src/main/java/com/product_api/controllers/ProductController.java
+++ src/main/java/com/product_api/controllers/ProductController.java
@@ -2,17 +2,29 @@ package com.product_api.controllers;
 
 import com.product_api.dto.ProductDTO;
 import com.product_api.entity.Product;
+import com.product_api.service.ProductService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @PostMapping("/add")
     public ResponseEntity<Product> addProduct(@RequestBody ProductDTO product){
+        return null;
+    }
 
+    @GetMapping("/products")
+    public List<Product> getAll(){
+        return productService.getAll();
     }
 }
