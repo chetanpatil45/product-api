fc18f79b14be0ace92ae4499573aec289aa8508b
Chetanpatil03 <chetanpatil6303@gmail.com>
2026-05-12 23:29:17 +0530

    mark swagger configuration file as configuration annotation

--- src/main/java/com/product_api/config/SwaggerConfiguration.java
+++ src/main/java/com/product_api/config/SwaggerConfiguration.java
@@ -2,9 +2,14 @@ package com.product_api.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.webmvc.api.OpenApiActuatorResource;
+import io.swagger.v3.oas.models.tags.Tag;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Configuration
 public class SwaggerConfiguration {
 
     @Bean
@@ -13,6 +18,10 @@ public class SwaggerConfiguration {
                 .info(
                         new Info().title("Product Management API's")
                                 .description("Product creation, updation, deletion")
+                ).tags(
+                        new ArrayList<>(
+                                Arrays.asList(new Tag().name("Product").description("Product related API's - CREATE, READ, UPDATE, DELETE"))
+                        )
                 );
     }
 }
